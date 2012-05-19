7ca82ace845c9046a6381a513b3850857e18804e
Patrick Kua <pkua@thoughtworks.com>
2012-05-19 18:04:42 +0100

    Fix bug with turn_off after rename. More documentation

--- notifier.rb
+++ notifier.rb
@@ -33,28 +33,33 @@ OFF = Colour.new(0, 0, 0)
 class EmailNotifier
   def initialize
     usb_context = LIBUSB::Context.new
+    # The hex value for Dream Cheeky on the device. Assuming only the email notifier is attached
     @device = usb_context.devices(:idVendor => 0x1d34).first
     reset_device
   end
 
   def colour!(colour)
     @colour = colour
-    send(colour.hex_string + "\x00\x00\x00\x00\x05")
+    send(colour.hex_string + "\x00\x00\x00\x00\x05")  # The 5 extra bytes apparently need to be sent
   end
 
   alias :colour :colour!
 
   def turn_off
-    set_colour(Colour.new(0, 0, 0))
+    colour!(Colour.new(0, 0, 0))
   end
 
   private
   def send(data)
     @device.open do |handle|
-      handle.control_transfer(:bmRequestType => 33, :bRequest => 0x09, :wValue => 0x200, :wIndex => 0x00, :dataOut => data)
+      request_type = LIBUSB::REQUEST_TYPE_CLASS | LIBUSB::RECIPIENT_INTERFACE  # flags to send data to device
+      # other special flags that I don't know what they do
+      handle.control_transfer(:bmRequestType => request_type, :bRequest => 0x09, :wValue => 0x200, :wIndex => 0x00, :dataOut => data)
     end
   end
 
+  # Special codes that apparently need to be sent to initialise the device.
+  # The first three values of the last line also represent the RGB colours on initialisation
   def reset_device
     send "\x1f\x02\x00\x2e\x00\x00\x2b\x03"
     send "\x00\x02\x00\x2e\x00\x00\x2b\x04"
